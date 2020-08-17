982424f59f9970ceabf4838a2e827353aac163c2
tunealog <tunealog@gmail.com>
2020-08-17 20:32:49 +0900

    Python Web Scraping

--- webscraping_basic/8_bs4_gauss.py.py
+++ webscraping_basic/8_bs4_gauss.py.py
@@ -12,6 +12,8 @@ res = requests.get(url)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml")
+
+# Titles and Links
 cartoons = soup.find_all("td", attrs={"class": "title"})
 
 # title = cartoons[0].a.get_text()
@@ -23,3 +25,14 @@ for cartoon in cartoons:
     title = cartoon.a.get_text()
     link = "https://comic.naver.com" + cartoon.a["href"]
     print(title, link)
+
+
+# Rates
+total_rates = 0
+cartoons = soup.find_all("div", attrs={"class": "rating_type"})
+for cartoon in cartoons:
+    rate = cartoon.find("strong").get_text()
+    print(rate)
+    total_rates += float(rate)
+print("Total : ", total_rates)
+print("Avg : ", total_rates / len(cartoons))
