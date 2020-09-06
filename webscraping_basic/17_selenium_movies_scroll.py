ffecbe28e4a5cfc71682cb998c9091184d3ba21f
tunealog <tunealog@gmail.com>
2020-09-06 21:30:32 +0900

    Python Web Scraping

--- webscraping_basic/17_selenium_movies_scroll.py
+++ webscraping_basic/17_selenium_movies_scroll.py
@@ -4,28 +4,7 @@
 # Date : 2020-09-05
 # Creator : tunealog
 
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "https://play.google.com/store/movies/top"
-# headers = {
-#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15", "Accept-Language": "ja-JP,ja"
-# }
-
-# res = requests.get(url, headers=headers)
-# res.raise_for_status()
-# soup = BeautifulSoup(res.text, "lxml")
-
-# movies = soup.find_all("div", attrs={"class": "WHE7ib mpg5gc"})
-# print(len(movies))
-
-# with open("movie.html", "w", encoding="utf8") as f:
-#     f.write(soup.prettify())
-
-# for movie in movies:
-#     title = movie.find("div", attrs={"class": "WsMG1c nnK0zc"}).get_text()
-#     print(title)
-
+from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 browser = webdriver.Chrome()
@@ -33,6 +12,7 @@ browser.maximize_window()
 
 # Move Page
 # url = "https://play.google.com/store/movies/top"
+url = "https://play.google.com/store/movies/collection/cluster?clp=0g4XChUKD3RvcHNlbGxpbmdfcGFpZBAHGAQ%3D:S:ANO1ljJvXQM&gsr=ChrSDhcKFQoPdG9wc2VsbGluZ19wYWlkEAcYBA%3D%3D:S:ANO1ljK7jAA"
 browser.get(url)
 
 # Down Scroll
@@ -55,3 +35,31 @@ while True:
     prev_height = curr_height
 
 print("Scroll End")
+
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+# movies = soup.find_all("div", attrs={"class": ["ImZGtf mpg5gc", "Vpfmgd"]})
+movies = soup.find_all("div", attrs={"class": "Vpfmgd"})
+print(len(movies))
+
+
+for movie in movies:
+    title = movie.find("div", attrs={"class": "WsMG1c nnK0zc"}).get_text()
+
+    original_price = movie.find("span", attrs={"class": "SUZt4c djCuy"})
+    # if original_price:
+    #     original_price = original_price.get_text()
+    # else:
+    #     continue
+
+    price = movie.find(
+        "span", attrs={"class": "VfPpfd ZdBevf i5DZme"}).get_text()
+
+    link = movie.find("a", attrs={"class": "JC71ub"})["href"]
+    print(f"Title : {title}")
+    print(f"Original Price : {original_price}")
+    print(f"Price : {price}")
+    print("Link : ", "https://play.google.com" + link)
+    print("-" * 100)
+browser.quit()
