cc07054fef36392a760baf7e5356985e36f760fe
tunealog <tunealog@gmail.com>
2020-09-07 22:00:23 +0900

    Python Web Scraping

--- webscraping_basic/19_headless_chrome_useragent.py
+++ webscraping_basic/19_headless_chrome_useragent.py
@@ -17,8 +17,6 @@ browser.maximize_window()
 url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent"
 browser.get(url)
 
-# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36
-# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/85.0.4183.83 Safari/537.36
 detected_value = browser.find_element_by_id("detected_value")
 print(detected_value.text)
 browser.quit()
