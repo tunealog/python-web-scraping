2c552a352c8cefa55ee1bc06deea3f88bc61d113
tunealog <tunealog@gmail.com>
2020-09-19 17:23:20 +0900

    Python Web Scraping

--- webscraping_project/project.py
+++ webscraping_project/project.py
@@ -58,8 +58,16 @@ def scrape_weather():
 
 def scrape_headline_news():
     print("[헤드라인 뉴스]")
-    url = "https://news.naver.com/"
+    url = "https://news.naver.com"
     soup = create_soup(url)
+    news_list = soup.find(
+        "ul", attrs={"class": "hdline_article_list"}).find_all("li", limit=3)
+    for index, news in enumerate(news_list):
+        title = news.div.a.get_text().strip()
+        link = url + news.find("a")["href"]
+        print("{}. {}".format(index+1, title))
+        print("  (링크 : {})".format(link))
+    print()
 
 
 if __name__ == "__main__":
